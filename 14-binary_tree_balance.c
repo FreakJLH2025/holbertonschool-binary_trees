ee16fc7367daa75c3634914a840b8cf2789102d5
FreakJLH2025 <joshua.luja@gmail.com>
2025-12-04 16:42:53 +0000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,39 +1,28 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
-/**
-* binary_tree_nodes - Counts nodes with at least 1 child (iterative DFS)
-* @tree: Pointer to the root node of the tree
-*
-* Return: Number of nodes with at least 1 child
-*/
-size_t binary_tree_nodes(const binary_tree_t *tree)
+static size_t height(const binary_tree_t *tree)
 {
-const binary_tree_t **stack;
-size_t top = 0, count = 0, capacity = 1024;
+size_t left, right;
 
 if (tree == NULL)
 return (0);
 
-stack = malloc(sizeof(binary_tree_t *) * capacity);
-if (stack == NULL)
-return (0);
+left = height(tree->left);
+right = height(tree->right);
 
-stack[top++] = tree;
-
-while (top > 0)
-{
-const binary_tree_t *node = stack[--top];
-
-if (node->left != NULL || node->right != NULL)
-count++;
-
-if (node->left)
-stack[top++] = node->left;
-if (node->right)
-stack[top++] = node->right;
+return ((left > right ? left : right) + 1);
 }
 
-free(stack);
-return (count);
+/**
+* binary_tree_balance - Measures the balance factor of a binary tree
+* @tree: Pointer to the root node of the tree
+*
+* Return: Balance factor, or 0 if tree is NULL
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return ((int)height(tree->left) - (int)height(tree->right));
 }
