795d62d9d446fdb17c91225851fe7656740c21c0
FreakJLH2025 <joshua.luja@gmail.com>
2025-12-04 18:19:13 +0000

    16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+static size_t height(const binary_tree_t *tree)
+{
+size_t left, right;
+
+if (tree == NULL)
+return (0);
+
+left = height(tree->left);
+right = height(tree->right);
+
+return ((left > right ? left : right)+1);
+}
+
+/**
+* binary_tree_is_perfect - Checks if a binary tree is perfect
+* @tree: Pointer to the root node
+*
+* Return: 1 if perfect, 0 otherwise
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+size_t left_h, right_h;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+
+if (tree->left == NULL || tree->right == NULL)
+return (0);
+
+left_h = height(tree->left);
+right_h = height(tree->right);
+
+if (left_h != right_h)
+return (0);
+
+return (binary_tree_is_perfect(tree->left) &&
+binary_tree_is_perfect(tree->right));
+}
